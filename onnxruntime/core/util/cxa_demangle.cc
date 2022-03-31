671c9d9ff6aa04212e2ae498ad9a3727c5ccdc16
Guangyun Han <guangyunhan@microsoft.com>
2022-03-31 20:12:39 +0800

    Add a simple dummy impl of __cxa_demangle

---
+++ onnxruntime/core/util/cxa_demangle.cc
@@ -0,0 +1,24 @@
+extern "C" {
+#include <stddef.h>
+#include <limits.h>
+
+char* __cxa_demangle(const char* mangled_name, char* output_buffer, size_t* length, int* status) {
+  if (!status) return NULL;
+  if (!mangled_name || !output_buffer || !length || (*length == 0) || (*length > INT_MAX)) {
+    *status = -3;
+    return NULL;
+  }
+
+  int i = 0;
+  int limit = (int)(*length) - 1;
+  for (; i < limit; i++) {
+    if (mangled_name[i] == '\0') break;
+    output_buffer[i] = mangled_name[i];
+  }
+  output_buffer[i] = '\0';
+
+  *status = 0;
+  return output_buffer;
+}
+
+}
