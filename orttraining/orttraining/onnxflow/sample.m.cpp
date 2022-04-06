3ed76116dbf214f6cc6a5ff25ba3321ca8bc0dc7
Baiju Meswani <bmeswani@microsoft.com>
2022-04-06 00:35:00 +0000

    On device training offline composition

---
+++ orttraining/orttraining/onnxflow/sample.m.cpp
@@ -0,0 +1,24 @@
+#include "orttraining/onnxflow/csrc/load_parameters.h"
+#include <filesystem>
+#include <iostream>
+#include <onnx/onnx_pb.h>
+
+int main()
+{
+    std::string path_to_parameters_proto;
+    std::cout << "Provide the absolute path to the parameters.of file\n";
+    std::cin >> path_to_parameters_proto;
+    std::filesystem::path path{path_to_parameters_proto};
+
+    auto parameters = onnxflow::load_parameters(std::filesystem::absolute(path).string());
+
+    std::cout << "The parameters are:\n";
+    for (const auto& param : parameters.parameters())
+    {
+        onnx::TensorProto tensor;
+        param.data().UnpackTo(&tensor);
+        std::cout << "<" << tensor.name() << ", requires_grad=" << (param.requires_grad() ? "True" : "False") << ">" << std::endl;
+    }
+
+    return 0;
+}
